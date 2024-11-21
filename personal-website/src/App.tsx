504c83e7924a6613652bb69cb432761afe4699f5
Hieu <hieun201104@gmail.com>
2024-11-21 15:31:55 -0800

    navbar on landing page

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -14,8 +14,8 @@ function App() {
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.key}>
           <Route element={<Computer />}>
-            <Route path="" element={<FirstPage />} />
             <Route element={<Nav />}>
+              <Route path="" element={<FirstPage />} />
               <Route path="content" element={<Content />} />
               <Route path="resume" element={<Resume />} />
             </Route>
